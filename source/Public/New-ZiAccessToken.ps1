6ba4e2877ecfe7366ab3d28019e67958372d1612
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-12-12 21:31:54 +0100

    Update New-ZiAccessToken.ps1

--- source/Public/New-ZiAccessToken.ps1
+++ source/Public/New-ZiAccessToken.ps1
@@ -1,22 +1,57 @@
-function New-ZIAccessToken {
+function New-ZiAccessToken {
+    [CmdletBinding()]
     param (
-        [string]$accessTokenPath = "$env:USERPROFILE\.creds\Zisson\zissonAccessToken.xml"
+        $AccessTokenPath = "$env:USERPROFILE\.creds\Zisson\ZissonAccessToken.xml",
+        $ApiKeyIdPath = "$env:USERPROFILE\.creds\Zisson\ZissonApiKeyId.xml",
+        $RefreshTokenPath = "$env:USERPROFILE\.creds\Zisson\ZissonRefreshToken.xml",
+        $CustomerGuidPath = "$env:USERPROFILE\.creds\Zisson\ZissonCustomerGuid.xml"
     )
 
-    $username = Read-Host "Enter API username"
-    $passwd = Read-Host -AsSecureString "Enter API password"
+    process {
+        #Create folder to store credentials
+        $AccessTokenDir = $AccessTokenPath.Substring(0, $AccessTokenPath.lastIndexOf('\'))
+        if (!(Test-Path $AccessTokenDir)) {
+            $null = New-Item -ItemType Directory $AccessTokenDir
+        }
 
-    $accessTokenDir = $accessTokenPath.Substring(0, $accessTokenPath.lastIndexOf('\'))
-    if (!(Test-Path $accessTokenDir)) {
-        #Create parent folders of the access token file
-        $null = New-Item -ItemType Directory $accessTokenDir
+        #Create ApiKeyId file
+        if (!(Test-Path $ApiKeyIdPath)) {
+            while (!$ApiKeyId) {
+                $ApiKeyId = Read-Host "Enter Zisson ApiKeyId"
+            }
+            $ApiKeyId | ConvertTo-SecureString -AsPlainText | Export-Clixml $ApiKeyIdPath
+        }
+
+        #Create RefreshToken file
+        if (!(Test-Path $RefreshTokenPath)) {
+            while (!$RefreshToken) {
+                $RefreshToken = Read-Host "Enter Zisson RefreshToken"
+            }
+            $RefreshToken | ConvertTo-SecureString -AsPlainText | Export-Clixml $RefreshTokenPath
+        }
+
+        #Create CustomerGuid file
+        if (!(Test-Path $CustomerGuidPath)) {
+            while (!$CustomerGuid) {
+                $CustomerGuid = Read-Host "Enter Zisson CustomerGuid"
+            }
+            $CustomerGuid | ConvertTo-SecureString -AsPlainText | Export-Clixml $CustomerGuidPath
+        }
+
+        #Request new accesstoken
+        $splat = @{
+            "Method" = "POST"
+            "Uri" = "https://app2.zisson.com/web-api/v1/authenticate/refresh-token"
+            "Header" = @{
+                "Accept" = "text/plain"
+                "Content-Type" = "application/json-patch+json"
+            }
+            "Body" = @{
+                "id" = Import-Clixml $ApiKeyIdPath | ConvertFrom-SecureString -AsPlainText
+                "customerGuid" = Import-Clixml $CustomerGuidPath | ConvertFrom-SecureString -AsPlainText
+                "refreshToken" = Import-Clixml $RefreshTokenPath | ConvertFrom-SecureString -AsPlainText
+            }
+        }
+        Invoke-RestMethod @splat
     }
-    
-    #Encode credentials to Base64
-    $text = "$($username):$($passwd | ConvertFrom-SecureString -AsPlainText)"
-    $bytes = [Text.Encoding]::UTF8.GetBytes($text)
-    $accessToken = [Convert]::ToBase64String($bytes)
-
-    #Create access token file
-    $accessToken | ConvertTo-SecureString -AsPlainText | Export-Clixml $accessTokenPath
 }
